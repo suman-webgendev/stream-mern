227a79ee0729f68af01b8829841bd8c521118255
SumanJS <sm7135120@gmail.com>
2024-08-28 19:29:00 +0530

    feat: added users datatable

--- server/public/scripts/users.js
+++ server/public/scripts/users.js
@@ -1,12 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const table = new DataTable("#videosTable", {
+  const table = new DataTable("#usersTable", {
     responsive: true,
     ordering: true,
     searching: true,
     paging: true,
     pageLength: 25,
     lengthMenu: [10, 25, 50, 100],
-    order: [[3, "desc"]],
-    columnDefs: [{ targets: 0, orderable: false }],
+    order: [[0, "desc"]],
   });
 });
