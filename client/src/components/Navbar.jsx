e5d6a9e5634c3509fadcb17f2b377ffa74b356bf
suman <suman.webgen@gmail.com>
2024-09-09 12:14:01 +0530

    feat: added docker start and onclick event on logo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
 
   return (
     <nav className="sticky left-0 right-0 top-0 z-[1000] flex h-[8vh] items-center justify-between bg-[rgba(0,0,0,.15)] px-6 dark:bg-white/20">
-      <div>
+      <div onClick={() => navigate("/")} className="cursor-pointer">
         <img src="/logo.svg" alt="logo" />
       </div>
 
